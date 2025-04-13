2e2ddf3670119ff447f90050ef4ff9d5d3ee65e7
Harrison Cramer <harrisoncramer@gmail.com>
2025-04-13 12:57:21 -0400

    Updated order_processor/order_failed.go

--- order_processor/order_failed.go
+++ order_processor/order_failed.go
@@ -12,7 +12,7 @@ type orderFailedAction struct{}
 func (a *orderFailedAction) Execute(ctx context.Context) (sm.Event, error) {
 	order, ok := ctx.Value("order").(*Order)
 	if !ok {
-		return "", fmt.Errorf("%w: order_failed did not have order", ErrBadExecutionContext, order)
+		return "", fmt.Errorf("%w: order_failed did not have order", ErrBadExecutionContext)
 	}
 	fmt.Printf("Order %s failed: %v\n", order.ID, order.Err)
 	return sm.NoOp, nil
